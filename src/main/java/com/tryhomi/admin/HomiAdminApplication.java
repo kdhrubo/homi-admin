611e4ce8d03d7eaf8ceb6748ffb4c1e6b48d3385
grabdoc <grabdoc2020@gmail.com>
2022-05-26 19:03:12 -0500

    first commit

---
+++ src/main/java/com/tryhomi/admin/HomiAdminApplication.java
@@ -0,0 +1,13 @@
+package com.tryhomi.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomiAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomiAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tryhomi/admin/HomiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryhomi.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomiAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
